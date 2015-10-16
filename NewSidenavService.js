b805a03ad49b238f20050b6df6e8f41e8e5da59b
Margarita Mayoral Villa <margaritamayoral@videotron.ca>
2015-10-16 16:05:17 -0400

    Create NewSidenavService.js

---
+++ NewSidenavService.js
@@ -0,0 +1,22 @@
+'use strict';
+angular
+	.module('NewApp')
+	.service('MainSidenavService', ['$http', '$log', '$timeout', '$q',  function($http, $log, $timeout, $q  ) {
+	var maintools = [{
+      name: 'home',
+      content: 'home.html'
+	  }, {
+      name: 'MyRepos'
+  }, {
+      name: 'FindUser'      
+  
+  }];
+
+  // Promise-based API
+  return {
+      loadAllTools: function() {
+          // Simulate async nature of real remote calls
+          return $q.when(maintools);
+      }
+  };
+}])
